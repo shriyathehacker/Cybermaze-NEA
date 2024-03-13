bb9a344c08dee4da9ddc45f47bd79540d79bef8c
Shriya (Ethan) <e.patel1@icloud.com>
2024-03-13 09:19:40 +0000

    Setup

--- menu.py
+++ menu.py
@@ -1,10 +1,11 @@
+print("Hello World")
+
 import pygame
 
 pygame.init()
 screen = pygame.display.set_mode((600, 600))
 
 while True:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      pygame.quit()
-      sys.exit()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
