76f7fadc9219fcd9207d465ea5b398274c568d98
Ethan <144679648+EthanPatel01000101@users.noreply.github.com>
2024-03-13 09:06:26 +0000

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,10 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((600, 600))
+
+while True:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      pygame.quit()
+      sys.exit()
